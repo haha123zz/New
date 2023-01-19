993ed0f446efe780094cbc59ddd58d346a59f906
haha123zz <86829374+haha123zz@users.noreply.github.com>
2023-01-19 10:00:56 -0500

    Update Smoothing.lua

--- Modules/Smoothing.lua
+++ Modules/Smoothing.lua
@@ -4,11 +4,13 @@ local Smoothing = {}; do
 
     local Values = SmoothingTab:AddSection("Values"); do
         Values:AddSlider("Track Percent", .1, 1.2, Smoothing.TrackPercent, {flag = "track_slider"}, function(int)
-            print(int)
+            Smoothing.TrackPercent = int
         end)
 
         Values:AddSlider("Flick Percent", .1, 1.2, Smoothing.FlickPercent, {flag = "flick_slider"}, function(int)
-            print(int)
+            Smoothing.FlickPercent = int
         end)
     end
 end
+
+return Smoothing
